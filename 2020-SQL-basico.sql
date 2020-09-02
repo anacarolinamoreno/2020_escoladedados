e0daa91e44e236953abd6704984833361b82124a
Ana Carolina Moreno <carolmoreno@gmail.com>
2020-09-02 02:18:55 -0300

    Add files via upload

--- 2020-SQL-basico.sql
+++ 2020-SQL-basico.sql
@@ -12,8 +12,6 @@
 */
 
 
-
-
 /* ### SELECT ### */
 
 /*
@@ -123,16 +121,16 @@ Ele vai no fim de uma coluna na sua lista de seleções.
 No nosso caso, a única coluna selecionada termina no END, portanto colocamos o AS depois disso:
 */
 
-SELECT  DISTINCT
+SELECT DISTINCT
 	CASE
 		WHEN uf = 'RO' THEN 'Rondônia'
 		ELSE 'não é Rondônia'
-	END AS Estado,
+	END AS Estado
 FROM ideb2017
 
 -- Mas podemos nomear mais colunas se quisermos, só é preciso lembrar de separá-las pela vírgula:
 
-SELECT  DISTINCT
+SELECT DISTINCT
 	CASE
 		WHEN uf = 'RO' THEN 'Rondônia'
 		ELSE 'não é Rondônia'
@@ -141,12 +139,15 @@ SELECT  DISTINCT
 FROM ideb2017
 
 
-/* ### WHERE ### */
+
 
 /*
 Os comandos acima servem para estabelecer condições de:
 - seleção de colunas (as variáveis da tabela);
 - tratamento dos nomes das colunas ou valores contidos nelas.
+*/
+
+/* ### WHERE ### 
 
 A seguir, veremos como funciona o WHERE, um comando importante para FILTRAR de quais linhas estamos falando.
 Para isso, vamos agora mexer um pouco na tabela do enem2019.
@@ -161,4 +162,245 @@ SELECT
 	NU_IDADE,
 	TP_SEXO
 FROM enem2019
-WHERE SG_UF_NASCIMENTO = 'RJ'
+WHERE SG_UF_NASCIMENTO = 'AC'
+
+/* IMPORTANTE: o WHERE sempre vai após o FROM, que sempre vai após o SELECT! 
+Pense nos três como orações diferentes numa mesma frase.
+Quando você passa de uma oração para outra, nunca pode separá-las por vírgula.
+*/
+
+-- Notou que acrescentamos um elemento novo, o sinal de "igual"? Ele é um dos chamados "operadores lógicos", que usamos para criar nossos filtros:
+
+/*
+LISTA DE OPERADORES LÓGICOS:
+- De comparação:
+	= (igual a)
+	<> (diferente de)
+	!= (diferente de)
+	< (menor que)
+	> (maior que)
+	<= (menor ou igual a)
+	>= (maior ou igual a)
+- Lógicos:
+	AND (uma condição E outra condição)
+	OR (uma OU outra condição)
+- Intervalo de valores:
+	BETWEEN (valores ENTRE x E y)
+- Pertencimento a conjuntos:
+	IN (contido em tal conjunto)
+	NOT IN (não contido em tal conjunto)
+- Buscar em parte do texto:
+	LIKE (texto inclui tal trecho)
+	NOT LIKE (texto não inclui tal trecho)
+- Nulabilidade:
+	IS NULL (célula daquela coluna, naquela linha, não foi preenchido)
+	IS NOT NULL (exclui as linhas com células sem preenchimento em uma coluna específica)
+	*/
+	
+-- Frase 6: "Selecionar as mesmas colunas do Acre que a frase 5, mas filtrando só pelos candidatos homens com mais de 50 anos."
+
+-- Tradução para o SQL:
+
+SELECT
+	SG_UF_NASCIMENTO,
+	NU_IDADE,
+	TP_SEXO
+FROM enem2019
+WHERE 
+	SG_UF_NASCIMENTO = 'AC' AND
+	TP_SEXO = 'M' AND
+	NU_IDADE > 50
+	
+-- Frase 7: "Selecionar as mesmas colunas do Acre que a frase 6, mas agora filtrando só pelas mulheres com idade entre 18 e 24 anos."
+
+SELECT
+	SG_UF_NASCIMENTO,
+	NU_IDADE,
+	TP_SEXO
+FROM enem2019
+WHERE 
+	SG_UF_NASCIMENTO = 'AC' AND
+	TP_SEXO = 'F' AND
+	(NU_IDADE BETWEEN 18 AND 24)
+	
+/* Notou que usamos parênteses no exemplo acima?
+O motivo é para evitar que o "AND" do intervalo selecionado pelo comando BETWEEN seja interpretado como uma mudança de comando.
+Para que o programa consiga entender exatamente o que você quer, é preciso prestar atenção nos detalhes.
+Veja em mais um exemplo abaixo:
+*/
+
+-- Frase 8: "Selecionar apenas candidatas mulheres no Acre que (tiveram nota de redação entre 800 e 1000) OU (tiveram nota 0 na redação)."
+
+-- Tradução para o SQL:
+
+SELECT
+	SG_UF_NASCIMENTO,
+	NU_IDADE,
+	TP_SEXO,
+	NU_NOTA_REDACAO
+FROM enem2019
+WHERE 
+	SG_UF_NASCIMENTO = 'AC' AND
+	TP_SEXO = 'F' AND
+	((NU_NOTA_REDACAO BETWEEN 800 AND 1000) OR
+	NU_NOTA_REDACAO = 0)
+	
+-- Frase 9: "Selecionar apenas candidatas mulheres no Acre que (tiveram nota 1000 na redação) OU (tiveram nota 0 na redação)."
+
+-- Tradução para o SQL:
+	
+SELECT
+	SG_UF_NASCIMENTO,
+	TP_SEXO,
+	NU_NOTA_REDACAO
+FROM enem2019
+WHERE 
+	SG_UF_NASCIMENTO = 'AC' AND
+	TP_SEXO = 'F' AND
+	(NU_NOTA_REDACAO = 1000 OR
+	NU_NOTA_REDACAO = 0)
+	
+/* Notou que alguns valores são colocados entre aspas e ficam vermelhos, enquanto outros vão sem aspas e ficam na cor preta?
+O motivo é que são TIPOS DE DADOS diferentes: uns são TEXT (texto), outros são INTEGER (números inteiros).
+O texto sempre vai entre aspas simples. Os números não precisam de aspas.
+Mas se você usar texto sem aspas, o código dá errado, porque o programa espera que valores foram de aspas tenham apenas números, não letras.
+Já qualquer dígito pode ser lido como texto.
+*/
+
+-- Teste na query abaixo:
+
+SELECT
+	SG_UF_NASCIMENTO,
+	NU_IDADE,
+	TP_SEXO
+FROM enem2019
+WHERE 
+	SG_UF_NASCIMENTO = AC AND
+	TP_SEXO = F AND
+	(NU_IDADE BETWEEN 18 AND 24)
+
+/*ALGUNS TIPOS DE DADOS:
+- TEXT: texto (aceita qualquer dígito)
+- CHAR(n): um campo de texto, mas tem definido um número fixo de caracterers (por exemplo: campo UF, com n = 2)
+- VARCHAR(n): um campo de texto com tamanho variável e que aceita até um número máximo de caracteres (por exemplo: campo para nome completo)
+- INTEGER: número inteiro
+- DECIMAL: número com casas decimais
+- REAL: também representa valores numéricos, mas com valores aproximados
+- BOOLEAN: traz os valores TRUE (verdadeiro) ou FALSE (falso)
+- DATE, TIMESTAMP: dados referentes a data e/ou hora
+*/
+
+-- DICA: O DB-Broswer costuma ler o tipo de dado de cada coluna, mas é possível alterá-lo manualmente.
+
+/* ### GROUP BY ###*/
+
+/*O comando GROUP BY serve para que você consiga agrupar os resultados de uma certa coluna para poder observar melhor seus dados.
+Para entender a sintaxe dele, é preciso enxergar as variávies que você lista dentro do comando SELECT pela ordem em que estão dispostas (posição 1, 2 etc.).*/
+
+/*FUNÇÕES DE AGRUPAMENTO: são usadas para contagem ou cálculos que consolidam todas as suas linhas em resumos.
+- COUNT: é usada para somar a quantidade de linhas em uma tabela ou para contar a soma de valores distintos de uma mesma coluna
+- SUM: soma de valores em uma coluna numérica
+- MAX: mostra o valor máximo existente em uma coluna numérica
+- MIN: mostra o valor mínimo existente em uma coluna numérica
+- AVG: vem de "average" ("média", em inglês) e calcula a média de todos os valores em uma coluna numérica
+*/
+
+-- Frase 10: "Contar quantas linhas tem a base enem2019."
+
+-- Tradução para o SQL:
+
+SELECT count(*) FROM enem2019
+
+-- Frase 11: "Contar quantos candidatos da base enem2019 são homens e quantos são mulheres."
+
+-- Tradução para o SQL:
+
+SELECT
+	TP_SEXO, 
+	count(*) AS QTDE_PORSEXO
+FROM enem2019
+GROUP BY TP_SEXO
+
+-- Frase 12: "Selecionar apenas candidatos e candidatas com nota mil na redação, e contar quantos são homens e quantos são mulheres."
+
+-- Tradução para o SQL:
+
+SELECT TP_SEXO, 
+	count(*) AS QTDE_NOTAMIL
+FROM enem2019
+WHERE NU_NOTA_REDACAO = 1000
+GROUP BY TP_SEXO
+
+/* ### ORDER BY ###*/
+
+/*O comando ORDER BY ordena seus resultados a partir dos valores de uma das colunas.
+A ordem crescente (ASC) é a padrão. Mas é possível alterá-la com uma função ao final do comando (DESC).*/
+
+-- Frase 13: "Selecionar quem fez o Enem no Acre por idade e sexo e ordená-los da maior pra menor nota na redação."
+
+-- Tradução para o SQL:
+
+SELECT 
+	SG_UF_NASCIMENTO,
+	NU_IDADE,
+	TP_SEXO,
+	NU_NOTA_REDACAO
+FROM enem2019
+WHERE 
+	SG_UF_NASCIMENTO = 'AC'
+ORDER BY
+	NU_NOTA_REDACAO DESC
+
+-- Frase 14: "Selecionar quem fez o Enem no Acre por idade e sexo e ordená-los da maior pra menor idade."
+
+-- Tradução para o SQL:
+
+SELECT 
+	SG_UF_NASCIMENTO,
+	NU_IDADE,
+	TP_SEXO,
+	NU_NOTA_REDACAO
+FROM enem2019
+WHERE 
+	SG_UF_NASCIMENTO = 'AC'
+ORDER BY
+	NU_IDADE DESC
+
+-- Frase 15: "Selecionar quem tirou nota mil na redação por UF e ordenar pela ordem alfabética do estado de trás para frente."
+
+-- Tradução para o SQL:
+
+SELECT 
+	SG_UF_NASCIMENTO,
+	NU_IDADE,
+	TP_SEXO,
+	NU_NOTA_REDACAO
+FROM enem2019
+WHERE NU_NOTA_REDACAO = 1000
+ORDER BY SG_UF_NASCIMENTO
+
+-- Frase 16: "Contar quantas notas mil cada UF teve e ordenar pela UF com o maior número de notas mil."
+
+-- Tradução para o SQL:
+
+SELECT 
+	SG_UF_NASCIMENTO,
+	NU_IDADE,
+	TP_SEXO,
+	COUNT(*) NU_NOTA_REDACAO
+FROM enem2019
+WHERE NU_NOTA_REDACAO = 1000
+GROUP BY SG_UF_NASCIMENTO
+ORDER BY NU_NOTA_REDACAO DESC
+
+-- Por fim, aplicamos o "Alias" para deixar a tabela ainda mais legível.
+
+SELECT 
+	SG_UF_NASCIMENTO AS Estado,
+	NU_IDADE AS Idade,
+	TP_SEXO AS Sexo,
+	COUNT(*) AS Notas_mil
+FROM enem2019
+WHERE NU_NOTA_REDACAO = 1000
+GROUP BY 1
+ORDER BY 4 DESC
